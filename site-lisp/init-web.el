b53d732791933767b86ca6996f427985094c5eb8
vritser <vritser@163.com>
2019-11-28 13:58:23 +0800

    rm xah fly key

---
+++ site-lisp/init-web.el
@@ -0,0 +1,32 @@
+(use-package css-mode
+  :ensure nil
+  :init (setq css-indent-offset 2))
+
+;; SCSS mode
+(use-package scss-mode
+  :init
+  ;; Disable complilation on save
+  (setq scss-compile-at-save nil))
+
+;; New `less-css-mde' in Emacs 26
+(unless (fboundp 'less-css-mode)
+  (use-package less-css-mode))
+
+;; CSS eldoc
+(use-package css-eldoc
+  :commands turn-on-css-eldoc
+  :hook ((css-mode scss-mode less-css-mode) . turn-on-css-eldoc))
+
+;; JSON mode
+(use-package json-mode)
+
+;; Major mode for editing web templates
+(use-package web-mode
+  :mode "\\.\\(phtml\\|php|[gj]sp\\|as[cp]x\\|erb\\|djhtml\\|html?\\|hbs\\|ejs\\|jade\\|swig\\|tm?pl\\|vue\\)$"
+  :config
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
+
+
+(provide 'init-web)
