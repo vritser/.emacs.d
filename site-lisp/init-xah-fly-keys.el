368b3a87bc9a66f114ddcc5d46bd066cfbfb4964
vritser <lisongwei@ipangcai.com>
2022-04-05 01:14:49 +0800

    xah-fly-keys: use submodule instead melpa & reconfigure it

--- site-lisp/init-xah-fly-keys.el
+++ site-lisp/init-xah-fly-keys.el
@@ -30,66 +30,54 @@
 
 ;;; Code:
 
+(require 'xah-fly-keys)
+(xah-fly-keys-set-layout "dvorak")
+(xah-fly-keys t)
 
-(use-package xah-fly-keys
+(use-package key-chord
   :ensure t
   :init
-  (require 'xah-fly-keys)
+  (key-chord-mode t)
   :config
-  (xah-fly-keys-set-layout "dvorak")
-  (xah-fly-keys 1)
+  (key-chord-define xah-fly-insert-map "th" 'xah-fly-command-mode-activate)
+  (global-set-key (kbd "<escape>") 'xah-fly-command-mode-activate))
 
-  (use-package key-chord
-    :ensure t
-    :init
-    (key-chord-mode 1)
-    :config
-    (key-chord-define xah-fly-insert-map "th" 'xah-fly-command-mode-activate)
-    (global-set-key (kbd "<escape>") 'xah-fly-command-mode-activate))
+(defun yuf/xah-fly-insert-map-hook ()
+  "Custome xah fly key insert map."
+  (define-key xah-fly-insert-map (kbd "C-r") 'v-open-line-indent)
+  (define-key xah-fly-insert-map (kbd "M-h") 'v-down-2-lines)
+  (define-key xah-fly-insert-map (kbd "C-n") 'next-line)
+  (define-key xah-fly-insert-map (kbd "C-a") 'beginning-of-line)
+  (define-key xah-fly-insert-map (kbd "<backspace>") 'hungry-delete-backward))
 
+(defun yuf/xah-fly-command-map-hook ()
+  "Custome xah fly key command map."
+  (define-key xah-fly-command-map (kbd "C-o") 'counsel-find-file)
+  (define-key xah-fly-command-map (kbd "a") 'counsel-M-x)
+  ;; (define-key xah-fly-command-map (kbd "C-n") 'xah-new-empty-buffer)
+  (define-key xah-fly-command-map (kbd "C-a") 'beginning-of-line)
+  (define-key xah-fly-command-map (kbd "b") 'swiper))
+
+(with-eval-after-load 'xah-fly-keys
+  (add-hook 'xah-fly-insert-mode-activate-hook 'yuf/xah-fly-insert-map-hook)
+  (add-hook 'xah-fly-command-mode-activate-hook 'yuf/xah-fly-command-map-hook)
+
+  ;; Keybindings
+
+  ;; global key map
+  (global-set-key (kbd "s-N") 'make-frame)
+  (global-set-key (kbd "s-n") 'xah-new-empty-buffer)
+
+  ;; visual regexp query/replace
   (define-key xah-fly-t-keymap (kbd "p") 'vr/replace)
   (define-key xah-fly-leader-key-map (kbd "p") 'vr/query-replace)
 
   ;; projectile key map
-  (define-prefix-command 'v-l-map)
-  (define-key v-l-map (kbd "f") 'projectile-find-file)
-  ;; (define-key v-l-map (kbd "f") 'counsel-git)
-  (define-key v-l-map (kbd "p") 'projectile-switch-project)
-  (define-key v-l-map (kbd "u") 'projectile-switch-to-buffer)
-
-  (define-key xah-fly-leader-key-map (kbd "l") v-l-map)
-
-  (defun v-fly-insert-map-hook ()
-    "Custome xah fly key insert map."
-    (define-key xah-fly-key-map (kbd "C-r") 'v-open-line-indent)
-    (define-key xah-fly-key-map (kbd "M-h") 'v-down-2-lines)
-    (define-key xah-fly-key-map (kbd "C-n") 'next-line)
-    (define-key xah-fly-key-map (kbd "C-a") 'beginning-of-line)
-    ;; (define-key xah-fly-key-map (kbd "g") nil)
-    ;; (define-key xah-fly-key-map (kbd "SPC-u") nil)
-
-    (define-key xah-fly-insert-map (kbd "<backspace>") 'hungry-delete-backward))
-
-  (defun v-fly-command-map-hook ()
-    "Custome xah fly key command map."
-    ;; (define-key xah-fly-key-map (kbd "SPC u") 'projectile-switch-to-buffer)
-    (define-key xah-fly-key-map (kbd "C-o") 'counsel-find-file)
-    (define-key xah-fly-key-map (kbd "a") 'counsel-M-x)
-    (define-key xah-fly-key-map (kbd "C-a") 'beginning-of-line)
-    (define-key xah-fly-key-map (kbd "b") 'swiper)
-    ;; (define-key xah-fly-key-map (kbd "r") 'forward-symbol)
-    ;; (define-key xah-fly-key-map (kbd "g") (lambda () (interactive) (forward-symbol -1)))
-    )
-
-
-  :hook ((xah-fly-insert-mode-activate . v-fly-insert-map-hook)
-         (xah-fly-command-mode-activate . v-fly-command-map-hook)))
-
-;; (require 'key-chord)
-;; (if (not (assq 'key-chord-mode minor-mode-alist))
-;;       (setq minor-mode-alist
-;; 	    (cons '(key-chord-mode " KeyC ")
-;; 		  minor-mode-alist)))
+  (define-prefix-command 'yuf-l-map)
+  (define-key yuf-l-map (kbd "f") 'projectile-find-file)
+  (define-key yuf-l-map (kbd "p") 'projectile-switch-project)
+  (define-key yuf-l-map (kbd "u") 'projectile-switch-to-buffer)
+  (define-key xah-fly-leader-key-map (kbd "l") yuf-l-map))
 
 (provide 'init-xah-fly-keys)
 ;;; init-xah-fly-keys.el ends here
