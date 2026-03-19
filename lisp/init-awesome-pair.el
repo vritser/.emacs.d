63a586f875f07d21453219eaaf97a45d9ef26161
vritser <vritser@gmail.com>
2026-03-19 15:15:31 +0800

    refactor(awesome-pair): simplify hook setup
    Use prog-mode-hook with eval-after-load instead of listing all
    individual mode hooks.

--- lisp/init-awesome-pair.el
+++ lisp/init-awesome-pair.el
@@ -28,40 +28,8 @@
 
 ;;; Code:
 
-(dolist (hook (list
-               'c-mode-common-hook
-               'c-mode-hook
-               'c++-mode-hook
-               'java-mode-hook
-               'scala-mode-hook
-               'haskell-mode-hook
-               'emacs-lisp-mode-hook
-               'lisp-interaction-mode-hook
-               'lisp-mode-hook
-               'maxima-mode-hook
-               'ielm-mode-hook
-               'sh-mode-hook
-               'makefile-gmake-mode-hook
-               'php-mode-hook
-               'python-mode-hook
-               'js-mode-hook
-               'go-mode-hook
-               'qml-mode-hook
-               'jade-mode-hook
-               'css-mode-hook
-               'ruby-mode-hook
-               'coffee-mode-hook
-               'rust-mode-hook
-               'qmake-mode-hook
-               'lua-mode-hook
-               'swift-mode-hook
-               'minibuffer-inactive-mode-hook
-               'typescript-mode-hook
-               'restclient-mode-hook
-               'dart-mode-hook
-               ))
-  (add-hook hook '(lambda () (awesome-pair-mode t))))
-
+(with-eval-after-load 'prog-mode
+  (add-hook 'prog-mode-hook #'(lambda () (awesome-pair-mode t))))
 
 (defun v-jump-pair-right ()
   "To right of next match parentheses."
