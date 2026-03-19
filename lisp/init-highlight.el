a5a03c6780e2c2c24c06bb6e2ad637808d65ea6f
vritser <vritser@gmail.com>
2026-03-19 15:15:45 +0800

    refactor(highlight): clean up and enable indent guides
    - Enable highlight-indent-guides in prog-mode
    - Fix typo: show-paren-shen -> show-paren-when
    - Remove commented-out code

--- lisp/init-highlight.el
+++ lisp/init-highlight.el
@@ -24,7 +24,6 @@
 
 ;;; Code:
 
-;; (setq beacon-color "#eb3594")
 (use-package hl-line
   :ensure nil
   :custom-face (hl-line ((t (:extend t))))
@@ -34,41 +33,21 @@
   :ensure nil
   :hook (after-init . show-paren-mode)
   :init (setq show-paren-when-point-inside-paren t
-	      show-paren-shen-point-in-periphery t))
+	      show-paren-when-point-in-periphery t))
 
-;; (use-package symbol-overlay
-;;   :diminish
-;;   :custom-face (symbol-overlay-default-face ((t (:inherit (region bold)))))
-;;   :init (setq symbol-overlay-idle-time 0.1
-;;               symbol-overlay-faces
-;;               '((:inherit (highlight bold))
-;;                 (:inherit (font-lock-builtin-face bold) :inverse-video t)
-;;                 (:inherit (warning bold) :inverse-video t)
-;;                 (:inherit (font-lock-constant-face bold) :inverse-video t)
-;;                 (:inherit (error bold) :inverse-video t)
-;;                 (:inherit (dired-mark bold) :inverse-video t)
-;;                 (:inherit (success bold) :inverse-video t)
-;;                 (:inherit (font-lock-keyword-face bold) :inverse-video t))))
-
-
-;; (use-package highlight-indent-guides
-;;   :hook (prog-mode . highlight-indent-guides-mode)
-;;   :init (setq highlight-indent-guides-method 'character
-;; 		highlight-indent-guides-auto-enabled t
-;; 		highlight-indent-guides-responsive 'top))
-
-;; (use-package volatile-highlights
-;;   :diminish
-;;   :hook (after-init . volatile-highlights-mode))
-
+(use-package highlight-indent-guides
+  :ensure nil
+  :hook (prog-mode . highlight-indent-guides-mode)
+  :init (setq highlight-indent-guides-method 'character
+		highlight-indent-guides-auto-enabled t
+		highlight-indent-guides-responsive 'top))
 
 (use-package paren-face
   :init (setq paren-face-regexp "[][(){}]")
   :hook (after-init . global-paren-face-mode))
 
-;; (use-package pulse
-;;   :ensure nil)
-
+(use-package pulse
+  :ensure nil)
 
 (provide 'init-highlight)
 ;;; init-highlight.el ends here
