b9e701044109ac04fa21243d91ae243098e3f8a4
vritser <vritser@gmail.com>
2026-03-19 15:15:00 +0800

    feat(project): migrate from projectile to built-in project.el
    - Remove projectile.el dependency
    - Update xah-fly-keys bindings to use project-find-file, project-switch-project
    - Add consult-ripgrep, consult-recent-file, consult-find to leader keymap
    - Add embark-act and embark-dwim bindings

--- lisp/init-xah-fly-keys.el
+++ lisp/init-xah-fly-keys.el
@@ -30,35 +30,16 @@
 
 ;;; Code:
 
-;; (setq xah-fly-use-control-key nil)
-(setq xah-fly-use-meta-key nil)
 (require 'xah-fly-keys)
 (xah-fly-keys-set-layout "dvorak")
 (xah-fly-keys t)
 
 (use-package key-chord
   :ensure t
-  :init
-  (key-chord-mode t)
+  :init (key-chord-mode t)
   :config
   (key-chord-define xah-fly-insert-map "th" 'xah-fly-command-mode-activate)
   (global-set-key (kbd "<escape>") 'xah-fly-command-mode-activate))
-
-(defun yuf/xah-fly-insert-map-hook ()
-  "Custome xah fly key insert map."
-  (define-key xah-fly-insert-map (kbd "C-r") 'v-open-line-indent)
-  (define-key xah-fly-insert-map (kbd "M-h") 'v-down-2-lines)
-  (define-key xah-fly-insert-map (kbd "C-n") 'next-line)
-  (define-key xah-fly-insert-map (kbd "C-a") 'beginning-of-line)
-  (define-key xah-fly-insert-map (kbd "<backspace>") 'hungry-delete-backward))
-
-(defun yuf/xah-fly-command-map-hook ()
-  "Custome xah fly key command map."
-  (define-key xah-fly-command-map (kbd "C-o") 'counsel-find-file)
-  (define-key xah-fly-command-map (kbd "a") 'counsel-M-x)
-  ;; (define-key xah-fly-command-map (kbd "C-n") 'xah-new-empty-buffer)
-  (define-key xah-fly-command-map (kbd "C-a") 'beginning-of-line)
-  (define-key xah-fly-command-map (kbd "b") 'swiper))
 
 (with-eval-after-load 'xah-fly-keys
   (add-hook 'xah-fly-insert-mode-activate-hook 'yuf/xah-fly-insert-map-hook)
@@ -70,16 +51,45 @@
   (global-set-key (kbd "s-N") 'make-frame)
   (global-set-key (kbd "s-n") 'xah-new-empty-buffer)
 
+  ;; projectile/consult key map (leader + l)
+  (define-prefix-command 'yuf-l-map)
+  (define-key yuf-l-map (kbd "f") 'project-find-file)
+  (define-key yuf-l-map (kbd "p") 'project-switch-project)
+  (define-key yuf-l-map (kbd "u") 'project-switch-to-buffer)
+  (define-key yuf-l-map (kbd "g") 'consult-ripgrep)
+  (define-key yuf-l-map (kbd "r") 'consult-recent-file)
+  (define-key yuf-l-map (kbd "d") 'consult-find)
+  (define-key xah-fly-leader-key-map (kbd "l") yuf-l-map)
+
   ;; visual regexp query/replace
-  ;;(define-key xah-fly-t-keymap (kbd "p") 'vr/replace)
+  (define-key xah-fly-leader-key-map (kbd "t p") 'vr/replace)
   (define-key xah-fly-leader-key-map (kbd "p") 'vr/query-replace)
 
-  ;; projectile key map
-  (define-prefix-command 'yuf-l-map)
-  (define-key yuf-l-map (kbd "f") 'projectile-find-file)
-  (define-key yuf-l-map (kbd "p") 'projectile-switch-project)
-  (define-key yuf-l-map (kbd "u") 'projectile-switch-to-buffer)
-  (define-key xah-fly-leader-key-map (kbd "l") yuf-l-map))
+  ;; switch buffer
+  (define-key xah-fly-leader-key-map (kbd "u") 'consult-buffer)
+
+  ;; embark context actions (leader + e)
+  (define-key xah-fly-leader-key-map (kbd "e") 'embark-act)
+  (define-key xah-fly-leader-key-map (kbd ".") 'embark-dwim))
+
+(defun yuf/xah-fly-insert-map-hook ()
+  "Custome xah fly key insert map."
+  (define-key xah-fly-insert-map (kbd "C-r") 'v-open-line-indent)
+  (define-key xah-fly-insert-map (kbd "M-h") 'v-down-2-lines)
+  (define-key xah-fly-insert-map (kbd "C-n") 'next-line)
+  (define-key xah-fly-insert-map (kbd "C-a") 'beginning-of-line)
+  (define-key xah-fly-insert-map (kbd "<backspace>") 'hungry-delete-backward))
+
+(defun yuf/xah-fly-command-map-hook ()
+  "Custome xah fly key command map."
+  ;; 文件查找（vertico 会自动接管 completion）
+  (define-key xah-fly-command-map (kbd "C-o") 'find-file)
+  ;; M-x 命令执行
+  (define-key xah-fly-command-map (kbd "a") 'execute-extended-command)
+  ;; (define-key xah-fly-command-map (kbd "C-n") 'xah-new-empty-buffer)
+  (define-key xah-fly-command-map (kbd "C-a") 'beginning-of-line)
+  ;; 行内搜索
+  (define-key xah-fly-command-map (kbd "b") 'consult-line))
 
 (provide 'init-xah-fly-keys)
 ;;; init-xah-fly-keys.el ends here
